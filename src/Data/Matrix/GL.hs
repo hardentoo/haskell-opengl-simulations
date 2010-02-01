0bd70ed699b86681b3ca6ea2183ffa825709b35c
James Halliday <substack@gmail.com>
2010-01-31 22:37:46 -0900

    dot product and vector code

--- src/Data/Matrix/GL.hs
+++ src/Data/Matrix/GL.hs
@@ -1,8 +1,27 @@
+{-# LANGUAGE MultiParamTypeClasses, FlexibleInstances #-}
+
 module Data.Matrix.GL where
 import Graphics.UI.GLUT
 
---(<.>) ::
---v1 <.> v2 = 
+class Num a => Vector t a where
+    (<.>) :: t a -> t a -> a -- dot product
+    v1 <.> v2 = sum $ zipWith (*) (fromVector v1) (fromVector v2)
+    
+    fromVector :: t a -> [a]
+    toVector :: [a] -> t a
+
+-- alias for <.>
+dot :: (Vector t a) => t a -> t a -> a
+dot = (<.>)
+
+-- most general version of dot product
+--instance Num b => Vector t a where
+
+instance Num a => Vector Vector3 a where
+    (Vector3 x1 y1 z1) <.> (Vector3 x2 y2 z2) = 
+        (x1 * x2) + (y1 * y2) + (z1 * z2)
+    fromVector (Vector3 x y z) = [x,y,z]
+    toVector (x:y:z:_) = Vector3 x y z
 
 vec3f :: Real a => a -> a -> a -> Vector3 GLfloat
 vec3f x y z = Vector3 (f x) (f y) (f z)
