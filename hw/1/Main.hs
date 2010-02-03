9f344740942ac2219e734dbee13358fb57727107
James Halliday <substack@gmail.com>
2010-02-02 22:47:48 -0900

    this version causes my kernel to go down

--- hw/1/Main.hs
+++ hw/1/Main.hs
@@ -29,29 +29,43 @@ instance Simulation SphereSim where
                 // translation is the third column of the inverse matrix
                 cameraPos = vec3(gl_ProjectionMatrixInverse[3]);
                 
-                point = vec3(gl_ModelViewMatrix * gl_Vertex);
-                gl_Position = gl_ProjectionMatrix
-                    * gl_ModelViewMatrix * gl_Vertex;
+                vec4 p = gl_ModelViewMatrix * gl_Vertex;
+                point = vec3(p);
+                gl_Position = gl_ProjectionMatrix * p;
             }
         |] [$here|
             // -- fragment shader
             varying vec3 cameraPos;
             varying vec3 point;
+            const float r = 0.5; // radius
+            
+            float solve(vec3 cam, vec3 ray) {
+                // solve for the intersection of a ray with the sphere
+                // mathematics shamelessly lifted from lecture notes
+                float a = dot(ray,ray);
+                float b = 2.0 * dot(ray,cam);
+                float c = dot(cam,cam) - r * r;
+                float det = b * b - 4.0 * a * c;
+                
+                if (det < 0.0) return -1;
+                float t = (-b - sqrt(det)) / (2.0 * a);
+                return t;
+            }
+            
             void main() {
                 vec3 ray = normalize(cameraPos - point);
-                // solve for the intersection of the ray with the sphere
-                // mathematics shamelessly lifted from lecture notes
-                float r = 0.5;
-                float a = dot(ray,ray);
-                float b = 2.0 * dot(ray,cameraPos);
-                float c = dot(cameraPos,cameraPos) - r * r;
-                float det = b * b - 4.0 * a * c;
-                // edges are noisy still somehow...
-                if (det < 0.0) discard;
-                float t = (-b - sqrt(det)) / (2.0 * a);
+                float t = solve(cameraPos,ray);
                 vec3 p = cameraPos + t * ray;
+                if (t < 0) discard;
                 if (dot(p,p) > r * r + 0.1) discard;
-                gl_FragColor = vec4(cameraPos,1);
+                
+                vec3 pnorm = normalize(p * vec3(2,-2,2));
+                if (dot(pnorm,ray) > 0.0) pnorm *= -1;
+                
+                vec3 lightSource = vec3(0,0,5);
+                
+                float v = dot(pnorm,lightSource);
+                gl_FragColor = vec4(v,v,v,1);
             }
         |]
         return $ sim { simShader = prog }
