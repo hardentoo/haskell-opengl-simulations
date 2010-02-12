34fba395385e5566d93c35e9b4a03528233867de
James Halliday <substack@gmail.com>
2010-02-11 21:37:40 -0900

    crazy replacement scheme compiles,but doesn't draw ellipse

--- hw/2/Main.hs
+++ hw/2/Main.hs
@@ -7,7 +7,8 @@ import Graphics.UI.GLUT.Simulation
 import Control.GL.Shader (newProgram,withProgram,bindProgram,here)
 import Control.Monad (forM_,liftM2)
 import Data.Bits (xor)
-import Foreign
+import Foreign (newArray)
+import Data.String.Utils (replace)
 
 -- With the simulator typeclass, just create your own datatypes...
 data EllipsoidSim = EllipsoidSim {
@@ -30,69 +31,83 @@ instance Simulation EllipsoidSim where
         return sim
     
     initSimulation sim = do
-        prog <- newProgram [$here|
-            // -- vertex shader
-            varying vec3 offset; // -- normalized vector offset of camera
-            varying vec3 camera; // -- camera in world coords
-            varying float depth;
-            
-            void main() {
-                // -- Translation is the third column of the projection inverse.
-                camera = vec3(gl_ProjectionMatrixInverse[3]);
-                
-                vec3 mv = vec3(gl_ModelViewMatrix * gl_Vertex);
-                offset = normalize(camera - mv);
-                
-                gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex; 
-                
-                vec4 proj = gl_ProjectionMatrix * vec4(mv,1);
-                depth = 0.5 + 0.5 * (proj.z / proj.w);
-            }
-        |] [$here|
-            // -- fragment shader
-            varying vec3 camera;
-            varying vec3 offset;
-            varying float depth;
-            
-            // -- Solve for the intersection of the ray with an ellipse
-            // -- described by ax² + by² + cz² = -k
-            float ellipse(vec4 eq, vec3 pos) {
-                float a = eq.x, b = eq.y, c = eq.z, k = -eq.w;
-                // -- P(t) = C + t * D, t >= 0
-                vec3 C = camera, D = offset;
-                
-                // -- a_, b_, and c_ used to compute quadratic equation
-                float a_ = (a * D.x * D.x) + (b * D.y * D.y) + (c * D.z * D.z);
-                float b_ = 2.0 * (
-                    (a * C.x * D.x) + (b * C.y * D.y) + (c * C.z * D.z)
-                );
-                float c_ = (a * C.x * C.x)
-                    + (b * C.y * C.y) + (c * C.z * C.z) + k;
-                
-                float alpha = 1.0;
-                // -- non-real answer detection
-                if ((b_ * b_) - (4 * a_ * c_) < 0.0) {
-                    return -1;
-                }
-                else {
-                    float t1 = (-b_ + sqrt(b_*b_ - 4*a_*c_)) / (2*a_*c_);
-                    float t2 = (-b_ - sqrt(b_*b_ - 4*a_*c_)) / (2*a_*c_);
-                    return min(t1,t2);
-                }
-            }
-            
-            void main() {
-                gl_FragDepth = depth;
-                vec4 e1 = vec4(1.0, 1.0, 3.0, 10.0);
-                vec3 p1 = vec3(0.0, 0.0, 0.0);
-                float t1 = ellipse(e1,p1);
-                if (t1 < 0.0) discard;
-                
-                gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
-            }
-        |]
+        prog <- newProgram vertexShader fragmentShader
         return $ sim { simShader = prog }
 
+vertexShader :: String
+vertexShader = [$here|
+    // -- vertex shader
+    varying vec3 offset; // -- normalized vector offset of camera
+    varying vec3 camera; // -- camera in world coords
+    varying float depth;
+    
+    void main() {
+        // -- Translation is the third column of the projection inverse.
+        camera = vec3(gl_ProjectionMatrixInverse[3]);
+        
+        vec3 mv = vec3(gl_ModelViewMatrix * gl_Vertex);
+        offset = normalize(camera - mv);
+        
+        gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex; 
+        
+        vec4 proj = gl_ProjectionMatrix * vec4(mv,1);
+        depth = 0.5 + 0.5 * (proj.z / proj.w);
+    }
+|]
+        
+-- My graphics card chokes on anything but the simplest functions.
+-- TODO: write a proper parser to automatically inline functions
+-- since the compiler is incapable of doing this properly
+ellipseF :: String
+ellipseF = [$here|
+    // -- Solve for the intersection of the ray with an ellipse
+    // -- described by ax² + by² + cz² = -k
+    {
+        float a = eq.x, b = eq.y, c = eq.z, k = -eq.w;
+        // P(t) = C + t * D, t >= 0
+        vec3 C = camera, D = offset;
+        
+        // a_, b_, and c_ used to compute quadratic equation
+        float a_ = (a * D.x * D.x) + (b * D.y * D.y) + (c * D.z * D.z);
+        float b_ = 2.0 * (
+            (a * C.x * D.x) + (b * C.y * D.y) + (c * C.z * D.z)
+        );
+        
+        float c_ = (a * C.x * C.x)
+            + (b * C.y * C.y) + (c * C.z * C.z) + k;
+        if (b_ * b_ < 4 * a_ * c_) {
+            t = -1.0; // non-real answer
+        }
+        else {
+            float t1 = (-b_ + sqrt(b_*b_ - 4*a_*c_)) / (2*a_);
+            float t2 = (-b_ - sqrt(b_*b_ - 4*a_*c_)) / (2*a_);
+            t = min(t1,t2);
+        }
+    }
+|]
+
+fragmentShader :: String
+fragmentShader = replace "$ellipse$" ellipseF [$here|
+    // -- fragment shader
+    varying vec3 camera;
+    varying vec3 offset;
+    varying float depth;
+    
+    void main() {
+        gl_FragDepth = depth;
+        
+        vec4 eq = vec4(1.0, 1.0, 1.0, 1.0);
+        vec3 pos = vec3(0.0, 0.0, 0.0);
+        
+        float t;
+        $ellipse$ // sets t (sigh)
+        
+        if (t < 0.0) discard;
+        
+        gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    }
+|]
+        
 -- Draw a floor, obviously!
 drawFloor :: EllipsoidSim -> IO ()
 drawFloor sim = do
