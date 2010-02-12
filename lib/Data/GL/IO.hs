cc3ccd13dec1ed560843cac426cf87ad6ba7397e
James Halliday <substack@gmail.com>
2010-02-11 18:16:14 -0900

    less annoying explicit types for textures

--- lib/Data/GL/IO.hs
+++ lib/Data/GL/IO.hs
@@ -25,3 +25,27 @@ color4fM x y z w = color $ color4f x y z w
 
 color4dM :: Real a => a -> a -> a -> a -> IO ()
 color4dM x y z w = color $ color4d x y z w
+
+texCoord1fM :: Real a => a -> IO ()
+texCoord1fM x = texCoord $ texCoord1f x
+
+texCoord1dM :: Real a => a -> IO ()
+texCoord1dM x = texCoord $ texCoord1d x
+
+texCoord2fM :: Real a => a -> a -> IO ()
+texCoord2fM x y = texCoord $ texCoord2f x y
+
+texCoord2dM :: Real a => a -> a -> IO ()
+texCoord2dM x y = texCoord $ texCoord2d x y
+
+texCoord3fM :: Real a => a -> a -> a -> IO ()
+texCoord3fM x y z = texCoord $ texCoord3f x y z
+
+texCoord3dM :: Real a => a -> a -> a -> IO ()
+texCoord3dM x y z = texCoord $ texCoord3d x y z
+
+texCoord4fM :: Real a => a -> a -> a -> a -> IO ()
+texCoord4fM x y z w = texCoord $ texCoord4f x y z w
+
+texCoord4dM :: Real a => a -> a -> a -> a -> IO ()
+texCoord4dM x y z w = texCoord $ texCoord4d x y z w
--- lib/Data/GL/Vector.hs
+++ lib/Data/GL/Vector.hs
@@ -4,7 +4,9 @@ module Data.GL.Vector (
     Vector(..), dot,
     vector3f, vector3d, vector4f, vector4d,
     vertex3f, vertex3d, vertex4f, vertex4d,
-    color3f, color3d, color4f, color4d
+    color3f, color3d, color4f, color4d,
+    texCoord1f, texCoord1d, texCoord2f, texCoord2d,
+    texCoord3f, texCoord3d, texCoord4f, texCoord4d
 ) where
 import Graphics.UI.GLUT
 
@@ -76,45 +78,77 @@ vector3f x y z = Vector3 (f x) (f y) (f z)
     where f = fromRational . toRational
 
 vector3d :: Real a => a -> a -> a -> Vector3 GLdouble
-vector3d x y z = Vector3 (f x) (f y) (f z)
-    where f = fromRational . toRational
+vector3d x y z = Vector3 (d x) (d y) (d z)
+    where d = fromRational . toRational
 
 vector4f :: Real a => a -> a -> a -> a -> Vector4 GLfloat
 vector4f x y z w = Vector4 (f x) (f y) (f z) (f w)
     where f = fromRational . toRational
 
 vector4d :: Real a => a -> a -> a -> a -> Vector4 GLdouble
-vector4d x y z w = Vector4 (f x) (f y) (f z) (f w)
-    where f = fromRational . toRational
+vector4d x y z w = Vector4 (d x) (d y) (d z) (d w)
+    where d = fromRational . toRational
 
 vertex3f :: Real a => a -> a -> a -> Vertex3 GLfloat
 vertex3f x y z = Vertex3 (f x) (f y) (f z)
     where f = fromRational . toRational
 
 vertex3d :: Real a => a -> a -> a -> Vertex3 GLdouble
-vertex3d x y z = Vertex3 (f x) (f y) (f z)
-    where f = fromRational . toRational
+vertex3d x y z = Vertex3 (d x) (d y) (d z)
+    where d = fromRational . toRational
 
 vertex4f :: Real a => a -> a -> a -> a -> Vertex4 GLfloat
 vertex4f x y z w = Vertex4 (f x) (f y) (f z) (f w)
     where f = fromRational . toRational
 
 vertex4d :: Real a => a -> a -> a -> a -> Vertex4 GLdouble
-vertex4d x y z w = Vertex4 (f x) (f y) (f z) (f w)
-    where f = fromRational . toRational
+vertex4d x y z w = Vertex4 (d x) (d y) (d z) (d w)
+    where d = fromRational . toRational
 
 color3f :: Real a => a -> a -> a -> Color3 GLfloat
 color3f x y z = Color3 (f x) (f y) (f z)
     where f = fromRational . toRational
 
 color3d :: Real a => a -> a -> a -> Color3 GLdouble
-color3d x y z = Color3 (f x) (f y) (f z)
-    where f = fromRational . toRational
+color3d x y z = Color3 (d x) (d y) (d z)
+    where d = fromRational . toRational
 
 color4f :: Real a => a -> a -> a -> a -> Color4 GLfloat
 color4f x y z w = Color4 (f x) (f y) (f z) (f w)
     where f = fromRational . toRational
 
 color4d :: Real a => a -> a -> a -> a -> Color4 GLdouble
-color4d x y z w = Color4 (f x) (f y) (f z) (f w)
+color4d x y z w = Color4 (d x) (d y) (d z) (d w)
+    where d = fromRational . toRational
+
+texCoord1f :: Real a => a -> TexCoord1 GLfloat
+texCoord1f x = TexCoord1 (f x)
     where f = fromRational . toRational
+
+texCoord1d :: Real a => a -> TexCoord1 GLdouble
+texCoord1d x = TexCoord1 (d x)
+    where d = fromRational . toRational
+
+texCoord2f :: Real a => a -> a -> TexCoord2 GLfloat
+texCoord2f x y = TexCoord2 (f x) (f y)
+    where f = fromRational . toRational
+
+texCoord2d :: Real a => a -> a -> TexCoord2 GLdouble
+texCoord2d x y = TexCoord2 (d x) (d y)
+    where d = fromRational . toRational
+
+texCoord3f :: Real a => a -> a -> a -> TexCoord3 GLfloat
+texCoord3f x y z = TexCoord3 (f x) (f y) (f z)
+    where f = fromRational . toRational
+
+texCoord3d :: Real a => a -> a -> a -> TexCoord3 GLdouble
+texCoord3d x y z = TexCoord3 (d x) (d y) (d z)
+    where d = fromRational . toRational
+
+texCoord4f :: Real a => a -> a -> a -> a -> TexCoord4 GLfloat
+texCoord4f x y z w = TexCoord4 (f x) (f y) (f z) (f w)
+    where f = fromRational . toRational
+
+texCoord4d :: Real a => a -> a -> a -> a -> TexCoord4 GLdouble
+texCoord4d x y z w = TexCoord4 (d x) (d y) (d z) (d w)
+    where d = fromRational . toRational
