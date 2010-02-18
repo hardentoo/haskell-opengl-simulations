abf47812413124651d91edb4503ef5702b1df74f
JH <substack@raven.(none)>
2010-02-18 04:38:57 -0900

    hw3 draws a sphere at least

--- hw/3/Main.hs
+++ hw/3/Main.hs
@@ -17,14 +17,15 @@ instance Simulation EllipsoidSim where
     navigator = wasd $ WASD { rSpeed = 0.001, tSpeed = 0.05 }
     
     display = do
-        drawFloor
         prog <- simShader <$> getSimulation
-        [x,y,z,w] <- map (!! 3) . toLists . inv . cameraMatrix <$> getCamera
-        liftIO $ bindProgram prog "camera" $ vertex3f x y z
-        
-        liftIO $ withProgram prog $ preservingMatrix $ do
-            color3fM 0 1 1
-            renderObject Solid $ Sphere' 2 6 24
+        mat <- cameraMatrix <$> getCamera
+        let [x,y,z] = take 3 $ toList $ mat <> (4 |> [0,0,0,1])
+        liftIO $ do
+            withProgram prog $ preservingMatrix $ do
+                bindProgram prog "camera" $ vertex3f x y z
+                --translateM $ vector3f 0 0 2
+                color3fM 0 1 1
+                renderObject Solid $ Sphere' 4 6 6
     
     begin = do
         ptr <- liftIO $ newArray $ take (19 * 19)
@@ -40,13 +41,12 @@ instance Simulation EllipsoidSim where
 vertexShader :: String
 vertexShader = [$here|
     // -- vertex shader
-    varying vec3 offset; // -- normalized vector offset of camera
-    uniform vec3 camera; // -- camera in world coords
+    uniform vec3 camera;
+    varying vec3 ray;
     
     void main() {
         vec3 mv = vec3(gl_ModelViewMatrix * gl_Vertex);
-        offset = normalize(camera - mv);
-        
+        ray = normalize(camera - mv);
         gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex; 
     }
 |]
@@ -55,34 +55,41 @@ fragmentShader :: String
 fragmentShader = [$here|
     // -- fragment shader
     uniform vec3 camera;
-    varying vec3 offset;
+    varying vec3 ray;
     
     void main() {
         // P(t) = C + t * D, t >= 0
         vec3 C = camera;
-        vec3 D = offset;
+        vec3 D = ray;
+        float r = 0.5; // radius
         
-        float a = 1.0, b = 1.0;
+        float a = dot(D,D);
+        float b = 2.0 * dot(D,C);
+        float c = dot(C,C) - r * r;
+        float det = b * b - 4.0 * a * c;
         
-        // x²/a + y²/b = 1
-        float i = b * D.x * D.x + a * D.y * D.y;
-        float j = 2.0 * (a * C.y * D.y + b * C.x * D.x);
-        float k = a * C.y * C.y + b * C.x * C.x - a * b;
-        
-        if (j * j < 4.0 * i * k) discard;
-        
-        float t1 = (-j + sqrt(j * j - 4 * i * k)) / (2 * i);
-        float t2 = (-j - sqrt(j * j - 4 * i * k)) / (2 * i);
-        float t = min(t1,t2);
-        
-        vec3 point = C + t * D; // point of intersection
-        if (point.z < 0.0) discard;
-        if (abs(point.y) > 0.2) discard;
-        
-        //vec4 proj = gl_ProjectionMatrix * vec4(vec3(point),1.0);
-        //gl_FragDepth = 0.1; // 0.5 + 0.5 * (proj.z / proj.w);
-        
-        gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+        if (det <= 0.0) {
+            gl_FragColor = vec4(0.0,0.0,0.0,1.0);
+        }
+        else {
+            float t = (-b - sqrt(det)) / (2.0 * a);
+            vec3 p = C + t * D;
+            
+            if (dot(p,p) > r * r + 0.01) discard;
+            
+            vec3 norm = normalize(p * vec3(2.0,-2.0,2.0));
+            vec3 lightSource = normalize(vec3(3.0,0.0,-5.0));
+            
+            // specular + diffuse lightning
+            float diffuse = min(max(dot(norm,lightSource),0.0),1.0);
+            
+            vec3 toCam = normalize(vec3(C)-p);
+            vec3 h = normalize(toCam + lightSource);
+            float spec = min(max(dot(norm,h),0.0),1.0);
+            
+            float v = min(max(pow(spec, 100.0) + diffuse, 0.0), 1.0);
+            gl_FragColor = vec4(v,v,v,1.0);
+        }
     }
 |]
  
